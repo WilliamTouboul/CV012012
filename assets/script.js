6908eaed200c593d40128c182b291adaa3d3489b
William Touboul <will.touboul@gmail.com>
2021-01-08 15:11:25 +0100

    Amélioration, section encore a completer ( sans Lorem Ipsum )

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+// function slideOnLoad() {
+//     setTimeout(function(){
+//         document.getElementById('elementToMove').style.left = '1000px'; // new left position is 1000px in this example
+//     }, 2000); // 2000 = 2 seconds after page load
+// }
